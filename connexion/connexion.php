d437daed5bb3d648d54cd6468682ced1cfdafbe7
OumaymaBrd <oumaymalovemaths>
2024-12-24 15:10:44 +0100

    Creation : Connexion bdd

---
+++ connexion/connexion.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'cabinet_medical'; 
+$username = 'root';       
+$password = '';           
+
+try {
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $conn->exec("SET NAMES utf8");
+    
+} catch(PDOException $e) {
+   
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
+
